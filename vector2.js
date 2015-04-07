30f9d64977fb4a11b9b068c2e04f87417edbf57f
EcentricTortoise <link.turnbull@gmail.com>
2015-04-07 16:16:37 +1000

    filled the vector file with some more info

--- vector2.js
+++ vector2.js
@@ -1,10 +1,53 @@
 var vector2 = function()
 {
-	this.set (x, y) = SCREEN_WIDTH/2, SCREEN_HEIGHT/2;
-	normalize();
-	{
-		add(v2);
-		subtract(v2);
-		multiplyScaler(1);
-	}
-};
+	this.x = 0;
+	this.y = 0;
+}
+
+Vector2.prototype.add = function( other_vector )
+{
+	this.x + other_vector.x;
+	this.y + other_vector.y;
+	
+	return result;
+}
+
+Vector2.prototype.subtract = function( other_vector )
+{
+	var result = new Vector2();
+	
+	result.x = this.x + other_vector.x;
+	result.y = this.y + other_vector.y;
+	
+	return result;
+}
+
+Vector2.prototype.multiplyScalar = function( scalar )
+{
+	var result = new Vector2();
+	
+	result.x = this.x * scalar;
+	result.y = this.y * scalar;
+	
+	return result;
+}
+
+Vector2.prototype.length = function()
+{
+	//sqrt (x*x + y*y)
+	
+	var result = Math.sqrt(this.x * this.x + this.y * this.y);
+	return result;
+}
+
+Vector2.prototype.normalize = function()
+{
+	var len = this.length();
+	
+	var result = new Vector2();
+	
+	result.x = this.x / len;
+	result.y = this.y / len;
+	
+	return result;
+}
